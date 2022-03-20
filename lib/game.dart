d7e5bfb49a08a7e32649d1387ce2210b9844aa5e
Mateus Leonardo <m4teusmatos@gmail.com>
2022-03-20 17:15:25 -0300

    finalizing interface the app

--- lib/game.dart
+++ lib/game.dart
@@ -15,18 +15,32 @@ class _GameState extends State<Game> {
         title: const Text("JokenPo"),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
-          Padding(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
               "Escolha do App",
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight:FontWeight.bold
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+          ),
+          Image.asset("images/padrao.png"),
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("images/pedra.png", height: 95),
+              Image.asset("images/papel.png", height: 95),
+              Image.asset("images/tesoura.png", height: 95),
+            ],
           )
         ],
       ),
